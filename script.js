d4a4082bf4b226c696ad44559a790ea54cc2dabc
PEDROHCA18 <114013463+PEDROHCA18@users.noreply.github.com>
2023-04-22 03:38:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,79 @@
-function validarSenha(){ 
-senha = document.getElementsByName('senha').value; 
-senha2 = document.getElementsByName('senha2').value; 
-if(senha!= senha2) { 	
-senha2.setCustomValidity("Senhas diferentes!"); 	
-} else { 	
-senha2.setCustomValidity(''); 
-} 
-senha.onchange = validatePassword; 
-senha2.onkeyup = validatePassword;
+ const date = document.getElementById('date') 
+
+ const pass = document.getElementById('pass') 
+
+ const passTwo = document.getElementById('passTwo') 
+
+  
+
+ function validate(item){ 
+
+  
+
+     item.setCustomValidity(''); 
+
+     item.checkValidity(); 
+
+  
+
+     //Verificador de idade 
+
+     if (item == date){ 
+
+         let hoje = new Date(); 
+
+         let dage = new Date(date.value); 
+
+  
+
+         let age = hoje.getFullYear() - dage.getFullYear(); 
+
+         let m = hoje.getMonth() - dage.getMonth(); 
+
+  
+
+         if(m < 0 || (m === 0 && hoje.getDate() < dage.getDate())){ 
+
+         age--; 
+
+         } 
+
+  
+
+         if(age >= 0) document.getElementById('date').value; 
+
+  
+
+         if(age > 130) item.setCustomValidity('Há algo errado com a sua idade!') 
+
+         else if (age >= 18) item.setCustomValidity(''); 
+
+         else item.setCustomValidity('você precisa ser maior de 18 anos!!'); 
+
+     } 
+
+  
+
+     //validação da segunda senha 
+
+     if(item == passTwo){ 
+
+         if(item.value === pass.value)setCustomValidity(''); 
+
+         else item.setCustomValidity('As senhas digitadas não são iguais.') 
+
+     } 
+
+ } 
+
+  
+
+  
+
+ //solitações de validações 
+
+ date.addEventListener('input', function(){validate(date) }); 
+
+ pass.addEventListener('input', function(){validate(pass)}); 
+
+ passTwo.addEventListener('input', function(){validate(passTwo)});

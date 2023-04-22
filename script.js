7db1dc2a0a5c1fc039677456a7d7043b33c2cb13
PEDROHCA18 <114013463+PEDROHCA18@users.noreply.github.com>
2023-04-22 03:30:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function validarSenha(){ 
+senha = document.getElementsByName('senha').value; 
+senha2 = document.getElementsByName('senha2').value; 
+if(senha!= senha2) { 	
+senha2.setCustomValidity("Senhas diferentes!"); 	
+} else { 	
+senha2.setCustomValidity(''); 
+} 
+senha.onchange = validatePassword; 
+senha2.onkeyup = validatePassword;
